4d4181fad1eb8e0544f401275632cbd1dcaaef47
SergeyVostokin <easts@mail.ru>
2023-03-16 20:24:32 +0400

    blchsym sample added

---
+++ samples/blchsym/blchsym.hpp
@@ -0,0 +1,209 @@
+/*$TET$$header*/
+/*--------------------------------------------------------------------------*/
+/*  Copyright 2023 Sergei Vostokin                                          */
+/*                                                                          */
+/*  Licensed under the Apache License, Version 2.0 (the "License");         */
+/*  you may not use this file except in compliance with the License.        */
+/*  You may obtain a copy of the License at                                 */
+/*                                                                          */
+/*  http://www.apache.org/licenses/LICENSE-2.0                              */
+/*                                                                          */
+/*  Unless required by applicable law or agreed to in writing, software     */
+/*  distributed under the License is distributed on an "AS IS" BASIS,       */
+/*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.*/
+/*  See the License for the specific language governing permissions and     */
+/*  limitations under the License.                                          */
+/*--------------------------------------------------------------------------*/
+
+#pragma once
+
+#include <iostream>
+#include <cstdlib>
+#include <time.h>
+
+#include <templet.hpp>
+#include <basesim.hpp>
+
+using namespace templet;
+using namespace std;
+
+const int DELAY = 1.0;
+const int NUM_WORKERS = 10;
+const int NUM_TASKS = 10;
+
+int EXEC_ORDER[NUM_WORKERS][NUM_TASKS];
+
+struct exec_order{
+    static void define(){
+        srand(time(NULL));    
+        for(int i=0; i<NUM_WORKERS; i++){
+            for(int j=0; j<NUM_TASKS; j++){
+redogen:                
+                EXEC_ORDER[i][j] = rand()%NUM_TASKS;
+                for(int k=0; k<j; k++) if(EXEC_ORDER[i][j]==EXEC_ORDER[i][k]) goto redogen;
+            }
+        }
+    }
+    static void print(){
+        srand(time(NULL));    
+        for(int i=0; i<NUM_WORKERS; i++){
+            for(int j=0; j<NUM_TASKS; j++){
+                cout << " " << EXEC_ORDER[i][j];
+            }
+            cout << endl;
+        }
+    }
+};
+
+class request : public templet::message {
+public:
+	brick(templet::actor*a=0, templet::message_adaptor ma=0) :templet::message(a, ma) {}
+    
+    int solved_task;
+    
+    int num_solved;
+    int solved_tasks[NUM_TASKS];  
+};
+
+/*$TET$*/
+
+#pragma templet bchain(in?message)
+
+struct bchain :public templet::actor {
+	static void on_in_adapter(templet::actor*a, templet::message*m) {
+		((bchain*)a)->on_in(*(message*)m);}
+
+	bchain(templet::engine&e) :bchain() {
+		bchain::engines(e);
+	}
+
+	bchain() :templet::actor(false)
+	{
+/*$TET$bchain$bchain*/
+         num_solved = 0;
+/*$TET$*/
+	}
+
+	void engines(templet::engine&e) {
+		templet::actor::engine(e);
+/*$TET$bchain$engines*/
+/*$TET$*/
+	}
+
+	inline void on_in(message&m) {
+/*$TET$bchain$in*/
+        m.send();
+/*$TET$*/
+	}
+
+	void in(message&m) { m.bind(this, &on_in_adapter); }
+
+/*$TET$bchain$$footer*/
+    int num_solved;
+    int solved_tasks[NUM_TASKS];  
+/*$TET$*/
+};
+
+#pragma templet !bworker(out!message,ready!message,t:basesim)
+
+struct bworker :public templet::actor {
+	static void on_out_adapter(templet::actor*a, templet::message*m) {
+		((bworker*)a)->on_out(*(message*)m);}
+	static void on_ready_adapter(templet::actor*a, templet::message*m) {
+		((bworker*)a)->on_ready(*(message*)m);}
+	static void on_t_adapter(templet::actor*a, templet::task*t) {
+		((bworker*)a)->on_t(*(templet::basesim_task*)t);}
+
+	bworker(templet::engine&e,templet::basesim_engine&te_basesim) :bworker() {
+		bworker::engines(e,te_basesim);
+	}
+
+	bworker() :templet::actor(true),
+		out(this, &on_out_adapter),
+		ready(this, &on_ready_adapter),
+		t(this, &on_t_adapter)
+	{
+/*$TET$bworker$bworker*/
+/*$TET$*/
+	}
+
+	void engines(templet::engine&e,templet::basesim_engine&te_basesim) {
+		templet::actor::engine(e);
+		t.engine(te_basesim);
+/*$TET$bworker$engines*/
+/*$TET$*/
+	}
+
+	void start() {
+/*$TET$bworker$start*/
+        out.send();
+/*$TET$*/
+	}
+
+	inline void on_out(message&m) {
+/*$TET$bworker$out*/
+        t.submit();    
+/*$TET$*/
+	}
+
+	inline void on_ready(message&m) {
+/*$TET$bworker$ready*/
+/*$TET$*/
+	}
+
+	inline void on_t(templet::basesim_task&t) {
+/*$TET$bworker$t*/
+        t.delay(DELAY);
+        ready.send();
+/*$TET$*/
+	}
+
+	message out;
+	message ready;
+	templet::basesim_task t;
+
+/*$TET$bworker$$footer*/
+/*$TET$*/
+};
+
+#pragma templet stopper(in?message)
+
+struct stopper :public templet::actor {
+	static void on_in_adapter(templet::actor*a, templet::message*m) {
+		((stopper*)a)->on_in(*(message*)m);}
+
+	stopper(templet::engine&e) :stopper() {
+		stopper::engines(e);
+	}
+
+	stopper() :templet::actor(false)
+	{
+/*$TET$stopper$stopper*/
+        num_stopped = 0;
+/*$TET$*/
+	}
+
+	void engines(templet::engine&e) {
+		templet::actor::engine(e);
+/*$TET$stopper$engines*/
+/*$TET$*/
+	}
+
+	inline void on_in(message&m) {
+/*$TET$stopper$in*/
+        
+        if(++num_stopped == num_total) stop();
+/*$TET$*/
+	}
+
+	void in(message&m) { m.bind(this, &on_in_adapter); }
+
+/*$TET$stopper$$footer*/
+    int num_stopped;
+    int num_total;
+/*$TET$*/
+};
+
+/*$TET$$footer*/
+/*$TET$*/
+
