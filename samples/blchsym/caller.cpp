42ca04d4f82585b361a9115bb7ff4660e839e9df
SergeyVostokin <easts@mail.ru>
2023-04-16 16:39:05 +0400

    fix

--- samples/blchsym/caller.cpp
+++ samples/blchsym/caller.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-	templet::everest_engine teng("vostokinsv", "SergeyVostokin1");
+	templet::everest_engine teng("--session-token--");
 
 	if (!teng) { cout << "There was an error connecting to Everest." << endl; return EXIT_FAILURE; }
 
