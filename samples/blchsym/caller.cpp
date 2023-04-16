03746fc4cd565178a65dfddcc9b7c6153317c8f3
SergeyVostokin <easts@mail.ru>
2023-04-16 18:54:02 +0400

    task run tested in caller

--- samples/blchsym/caller.cpp
+++ samples/blchsym/caller.cpp
@@ -5,11 +5,32 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-	templet::everest_engine teng("--session-token--");
+	templet::everest_engine teng("ypoudxeb5wvz41uzv6roh5q58t2qgrf9bqzy1mmhbawy5s3um2h6muxkyx30imm2");
+    
+    templet::everest_task task(teng,"643a76ae14000084cc249ad7");
+    
+    json in;    
+	in["name"] = "blockch-application";
+	
+    
+    for(int i=0; i<10; i++){
 
-	if (!teng) { cout << "There was an error connecting to Everest." << endl; return EXIT_FAILURE; }
+        in["inputs"]["task"] = i;
+        task.submit(in);
 
-	cout << "Success!!!";
+        teng.run();
 
-	return EXIT_SUCCESS;
+        templet::everest_error cntxt;
+
+        if (teng.error(&cntxt)) {
+            teng.print_error(&cntxt);
+            return EXIT_FAILURE;
+        }
+
+        json out = task.result();
+
+        cout << out["completed_tasks"] << endl;
+    }
+    
+    return EXIT_SUCCESS;
 }
