1647a715dd77b52a20cba02f0444d8eab4001443
Сергей Востокин <SergeyVostokin@users.noreply.github.com>
2023-12-02 19:11:24 +0400

    Update start4python2.sh

--- bin/start4python2.sh
+++ bin/start4python2.sh
@@ -8,11 +8,12 @@ PID_FILE=agent.pid
 if [ -f $PID_FILE ] && [ -f /proc/$(cat $PID_FILE)/status ]; then
    echo "Agent is already running"
 else
+     PYTHON=python2
 #    if which python3 > /dev/null; then
 #        PYTHON=python3
 #    else
         # Find latest Python 2.x version
-        PYTHON=$(ls /usr/bin/python2.? | tail -n1)
+#       PYTHON=$(ls /usr/bin/python2.? | tail -n1)
 #    fi
     # Make sure Python is 2.6 or later
     PYTHON_OK=`$PYTHON -c 'import sys; print (sys.version_info >= (2, 6) and "1" or "0")'`
