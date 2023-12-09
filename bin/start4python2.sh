61eed98ed25f2cb0bfe0747dd57e9608c3d20c7a
Сергей Востокин <SergeyVostokin@users.noreply.github.com>
2023-12-09 12:47:01 +0400

    Update start4python2.sh

--- bin/start4python2.sh
+++ bin/start4python2.sh
@@ -8,13 +8,15 @@ PID_FILE=agent.pid
 if [ -f $PID_FILE ] && [ -f /proc/$(cat $PID_FILE)/status ]; then
    echo "Agent is already running"
 else
-     PYTHON=python2
-#    if which python3 > /dev/null; then
-#        PYTHON=python3
-#    else
+    #if which python3 > /dev/null; then
+    #    PYTHON=python3
+    #else
         # Find latest Python 2.x version
-#       PYTHON=$(ls /usr/bin/python2.? | tail -n1)
-#    fi
+    #    PYTHON=$(ls /usr/bin/python2.? | tail -n1)
+    #fi
+    
+    PYTHON=/srv/conda/envs/everest/bin/python2
+    
     # Make sure Python is 2.6 or later
     PYTHON_OK=`$PYTHON -c 'import sys; print (sys.version_info >= (2, 6) and "1" or "0")'`
     if [ "$PYTHON_OK" = '0' ]; then
