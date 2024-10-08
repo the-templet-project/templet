7594679527fbf715fa868b3cedad6012f18f4181
Сергей Востокин <SergeyVostokin@users.noreply.github.com>
2024-10-08 20:10:51 +0400

    Add files via upload

--- samples/a022008/task-bag.hpp
+++ samples/a022008/task-bag.hpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <list>
 #include <vector>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
@@ -32,35 +34,47 @@ public:
 class bag_par{
 
 protected:
-    class task{  
-        friend class bag_par; 
+    class task{
+    public:
+        virtual ~task()=0; 
         virtual void on_run()=0;
     }; 
     
-    virtual task* create_task()=0; 
+    virtual task* create_task()=0;
+    
     
     virtual bool on_get(task&)=0;
     virtual void on_put(task&)=0;
-
-    void init(){
-        for(int i=0; i<num_workers; i++)
-            ready_task_list.push_back(create_task());
-    }
 
 public:
     bag_par(int _num_workers){ num_workers = _num_workers; }
 
     void run(){
-        init();
+        srand((unsigned)time(0));
+        cur_num_workers = 0;
         task* t=create_task();
-        while(on_get(*t)){
-            t->on_run();
-            on_put(*t);
+
+        for(;;){
+            while(on_get(*t) && (cur_num_workers < num_workers)){
+                planned_task_arr.push_back(t);
+                cur_num_workers++;
+            }
+            
+            unsigned size; 
+            if((size = planned_task_arr.size())==0) break;
+            
+            unsigned selected = rand() % size;
+            task* selected_task = planned_task_arr[selected];
+            planned_task_arr.erase(planned_task_arr.begin()+selected);
+
+            selected_task->on_run();
+            on_put(*selected_task);
+            delete selected_task;
         }
     }
 
 private:
+    int cur_num_workers;
     int num_workers;
     vector<task*> planned_task_arr;
-    list<task*>   ready_task_list;
 };
