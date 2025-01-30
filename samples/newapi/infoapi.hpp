849b325cd17fae0d9aa3b5214a0316c97ce572df
Сергей Востокин <SergeyVostokin@users.noreply.github.com>
2025-01-30 18:49:57 +0400

    info api ready

--- samples/newapi/infoapi.hpp
+++ samples/newapi/infoapi.hpp
@@ -13,21 +13,66 @@
 /*  See the License for the specific language governing permissions and     */
 /*  limitations under the License.                                          */
 /*--------------------------------------------------------------------------*/
+#include <vector>
+#include <iostream>
 #include "baseapi.hpp"
 
+class Transaction;
+
 class DataObject{
+friend class Transaction;
 public:
-    DataObject(unsigned pid, EventLog&_log):log(_log),PID(pid){}
-    void update(){}
+    DataObject(unsigned pid, EventLog&_log):log(_log),PID(pid),next_to_read_event(0){}
+    void update();
+private:
+    unsigned register_transaction(Transaction*tr){
+        unsigned tag = transactions.size();
+        transactions.push_back(tr);
+        return tag;
+    }
 private:
     EventLog& log;
-    unsigned PID;    
+    unsigned PID;
+private:
+    vector<Transaction*> transactions;
+    unsigned next_to_read_event;
 };
 
 class Transaction{
+friend class DataObject;
 public:
-    Transaction(DataObject&){}
-    void run(){}
+    Transaction(DataObject&obj):data_object(obj){tag = obj.register_transaction(this);}
+    void run(){
+        BLOB blob; ostringstream out;
+        
+        on_save(out);
+        blob = out.rdbuf()->str();
+       
+        data_object.log.write(tag,data_object.PID,blob);
+        data_object.update();
+    }
 protected:
     virtual void on_run()=0;
+    virtual void on_save(ostream&out)=0;
+    virtual void on_load(istream&in)=0;
+private:
+    unsigned tag;
+    DataObject& data_object;
 };
+
+void DataObject::update(){
+    for(;;){
+        unsigned tag; unsigned pid; BLOB blob;
+        if(!log.read(next_to_read_event,tag,pid,blob))return;
+
+        Transaction* next_transaction = transactions[tag];
+
+        istringstream in;
+        in.rdbuf()->str(blob);
+        
+        next_transaction->on_load(in);
+        next_transaction->on_run();
+        
+        next_to_read_event++;
+    }
+}
