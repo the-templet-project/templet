310a58a003c49e21d9f0b706b68421c433dfa92d
SergeyVostokin <easts@mail.ru>
2026-05-04 23:43:12 +0400

    a305570 updated

--- samples/a305570/mapmain.hpp
+++ samples/a305570/mapmain.hpp
@@ -1,6 +1,9 @@
 #include <vector>
 #include <iostream>
 #include <sstream>
+#include <cstdlib>
+#include <fstream>
+#include <cstdio>
 
 class mapper{
 public:
@@ -49,22 +52,105 @@ public:
 };
 
 class dls7_mapper: public mapper{
+static const unsigned DIM=7;
 public:
-    dls7_mapper(mapper::engine& eng): mapper(eng){}
+    dls7_mapper(mapper::engine& eng,const std::string&fdir,unsigned nchunks):
+        mapper(eng),filedir(fdir),num_of_chunks(nchunks){
+            for(int i=0; i<num_of_chunks; i++) tasks.push_back(std::pair(i,i));
+            for(int i=0; i<num_of_chunks; i++)
+            for(int j=i+1; j<num_of_chunks; j++)tasks.push_back(std::pair(i,j));
+        }
+public:
+    struct sq7x7{unsigned sq[DIM*DIM];};
+    std::list<std::pair<sq7x7,sq7x7>> orto_dls7_final;
 private:
     void on_init(unsigned size) override{
-        N.resize(size); NxN.resize(size);
+       tasks.resize(num_of_chunks*(1+num_of_chunks)/2);
     }
     void on_map(unsigned id) override{
-        NxN[id] = N[id] * N[id];
+        if(tasks[id].first==tasks[id].second){
+            load(first_dls7_chunk,tasks[id].first);
+            find_orto_in_one();
+        }
+        else{
+            load(first_dls7_chunk,tasks[id].first);
+            load(second_dls7_chunk,tasks[id].second);
+            find_orto_in_two();
+        }
     }
     void on_save(unsigned id, std::ostream& out, bool mapped) override{
-        if(mapped) out << NxN[id]; else out << N[id];  
+          
     }
 	void on_load(unsigned id, std::istream& in, bool mapped) override{
-        if(mapped) in >> NxN[id]; else in >> N[id];
+        
     }
-public:
-    std::vector<int> N;
-    std::vector<int> NxN;
+private:
+    void load(std::vector<sq7x7>& chunk, unsigned chunk_id){
+        chunk.clear();
+
+        char name_buf[10];
+		sprintf(name_buf, "nls%.2d.txt", chunk_id);
+        std::string chunk_file_name = filedir + name_buf;
+        
+        std::ifstream file(chunk_file_name);
+    	std::cout << "loading file " << chunk_file_name << std::endl;
+    
+    	if (file) {
+    		int k;
+    		while(!file.eof()){
+    			sq7x7 sqware; unsigned digit;
+    			for (int i = 0; i < DIM; i++) for (int j = 0; j < DIM; j++) {
+    				if (file >> digit)  sqware.sq[i*DIM + j] = digit;
+    				else {
+    					if (i == 0 && j == 0) 
+    						std::cout << "file " << name_buf << " loaded" << std::endl;
+    					else {
+    						std::cout << "Error!!! Bad file";
+    						exit(EXIT_FAILURE);
+    					}
+    				}
+    			}
+                chunk.push_back(sqware);
+    		}
+    	}
+    	else {
+    		std::cout << "Error!!! Cannot open file";
+    		exit(EXIT_FAILURE);
+    	}
+    }
+    void find_orto_in_one(){
+        orto_dls7_local.clear();
+        for (int m1 = 0; m1 < first_dls7_chunk.size(); m1++)
+			for (int m2 = m1 + 1; m2 < first_dls7_chunk.size(); m2++)
+				if (is_ortogonal_pair(first_dls7_chunk[m1],first_dls7_chunk[m2])) {
+            		orto_dls7_local.push_back(std::pair(first_dls7_chunk[m1],first_dls7_chunk[m2]));
+				}
+    }
+    void find_orto_in_two(){
+        orto_dls7_local.clear();
+        for (int m1 = 0; m1 < first_dls7_chunk.size(); m1++)
+			for (int m2 = 0; m2 < second_dls7_chunk.size(); m2++)
+				if (is_ortogonal_pair(first_dls7_chunk[m1],second_dls7_chunk[m2])) {
+            		orto_dls7_local.push_back(std::pair(first_dls7_chunk[m1],second_dls7_chunk[m2]));
+				}
+    }
+    static bool is_ortogonal_pair(sq7x7 f, sq7x7 s)
+    {
+    	unsigned my_set[DIM*DIM];
+    	int cur_elem = 0;
+    	for (int i = 0; i < DIM*DIM; i++) {
+    		unsigned inserted = f.sq[i] * DIM + s.sq[i];
+    		for (int j = 0; j < cur_elem; j++)
+    			if (my_set[j] == inserted) return false;
+    		my_set[cur_elem++] = inserted;
+    	}
+    	return true;
+    }
+private:
+    std::string filedir;
+    unsigned num_of_chunks;
+    std::vector<std::pair<unsigned,unsigned>> tasks;
+    std::list<std::pair<sq7x7,sq7x7>> orto_dls7_local;
+    std::vector<sq7x7> first_dls7_chunk;
+    std::vector<sq7x7> second_dls7_chunk;
 };
