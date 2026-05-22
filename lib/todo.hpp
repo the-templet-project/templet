a878506aa4b96eca02d39af2a486ce1f05faca08
SergeyVostokin <easts@mail.ru>
2026-05-22 21:01:02 +0400

    todo updated

--- lib/todo.hpp
+++ lib/todo.hpp
@@ -1,24 +1,3 @@
-/*
-new programming models/code names:
-==================================
-
-actor task model -- templet::acta + templet::meta::acta
-(in templet::acta::actor  use 'task','ask','ret','say';
- in templet::acta::message use bool operator() <- access
-)
-
-global object model -- templet::globj + templet::meta::globj
-(in templet::globj use 'update')
-
-async-await model -- templet::async
-(in templet::async use 'task', 'wait')
-
-ask-say model- templet::chat
-(in templet::chat use 'ask', 'say')
-
-parallel map - templet::map
-(in templet::map use 'on_run(unsigned iter)','on_save','on_load','on_init(unsigned size)')
-*/
 #include <functional>
 #include <istream>
 #include <ostream>
@@ -48,20 +27,131 @@ namespace templet {
 		map(wal&);
 		void init(unsigned size);
 		void operator()(
-			std::function<void(unsigned size)>init,
-			std::function<void(unsigned iter)>map,
-			std::function<void(unsigned iter, std::istream&, bool mapped)>save
-			= [](unsigned, std::istream&, bool) {},
-			std::function<void(unsigned iter, std::ostream&, bool mapped)>load
-			= [](unsigned, std::ostream&, bool) {}
+			std::function<void(unsigned size)> init,
+			std::function<void(unsigned iter)> map,
+			std::function<void(unsigned iter, std::ostream&, bool mapped)> save
+			= [](unsigned, std::ostream&, bool) {},
+			std::function<void(unsigned iter, std::istream&, bool mapped)> load
+			= [](unsigned, std::istream&, bool) {}
 		);
 	};
 
 	class procpool {
 	public:
 		procpool(unsigned size, wal&);
-		void operator()(std::function<unsigned pid, wal&) > process);
+		void operator()(std::function<unsigned pid, wal&>) process);
 		void delay(double seconds);
 		double duration();
 	};
+
+    class globj{
+    public:
+        globj(wal&);
+        void init();
+    public:
+        virtual void on_init() = 0; 
+    public:
+        void update(
+            unsigned id,
+            std::function<void(std::ostream&)> save,
+            std::function<void(std::istream&,std::ostream&)> update,
+            std::function<void(std::istream&)> load
+        );
+        void update(
+            unsigned id,
+            std::function<void(std::ostream&)> update,
+            std::function<void(std::istream&)> load
+        );
+        void update(
+            unsigned id,
+            std::function<void(std::ostream&)> save,
+            std::function<void(std::istream&)> update
+        );
+        void update();
+    };
+
+namespace meta{
+
+    class globj{
+    public:
+        void name(const char name[]);
+		void prefix(const char prefix[]);
+        update& def(const char name[]);
+        class update{
+            update& in(const char param[],const char stub_value[]="",const char stub_value_def[]="");
+            update& out(const char param[],const char stub_value[]="",const char stub_value_def[]="");
+            update& constant();
+        };
+    };
+}
+    class async{
+    public:
+        async(wal&);
+        void task(bool local,
+            std::function<void(std::ostream&)> action,
+            std::function<void(std::istream&)> load
+        );
+        void wait();
+    };
+
+    class chat{
+    public:
+        chat(wal&);
+        void chat(const std::string& user);
+        virtual void on_chat(const std::string& user) = 0;
+        void say(std::function<void()>output_action);
+        void ask(
+            std::function<void(std::ostream&)>input_action,
+            std::istream& input
+        ); 
+    }
+
+    class acta{
+    public:
+        acta(wal&);
+        void run();
+        virtual on_run() 
+    public:
+        class actor{
+        public:
+            actor(acta&);
+            actor(acta*);
+        public:
+            void ask(message&);
+            void ask(message*);
+            void ret(message&);
+            void ret(message*);
+            void say(message&);
+            void say(message*);
+        public:
+             void task(bool local,
+                std::function<void(std::ostream&)> action,
+                std::function<void(std::istream&)> load
+            );
+        };
+        class message{
+        public:
+            message(actor&);
+            message(actor*);
+        public:
+            bool operator(actor*)();
+            bool operator(actor&)();
+        };
+    };
+
+namespace meta{
+
+    class acta{
+    public:
+        void name(const char name[]);
+		void prefix(const char prefix[]);
+        actor& def(const char name[]);
+    public:
+        class actor{
+            actor& in(const char type[],const char name[]);
+            actor& out(const char type[],const char name[]);
+            actor& task(const char name[]);
+        }
+    };
+}
 }
