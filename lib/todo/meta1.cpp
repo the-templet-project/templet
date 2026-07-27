9d46916766e803739f29ed03bbeda2095a205bdf
SergeyVostokin <easts@mail.ru>
2026-07-27 20:50:16 +0400

    meta samples updated

--- lib/todo/meta1.cpp
+++ lib/todo/meta1.cpp
@@ -1,4 +1,4 @@
-#pragma templet  header
+#pragma templet +header
 
 #include "meta.hpp"
 #include "globj.hpp"
@@ -20,50 +20,50 @@ public:
             .in("int nxn");
     }
 };
-#pragma templet /header
+#pragma templet -header
 
 class bag_of_tasks:public templet::globj{
 public:
     bag_of_tasks(templet::wal&l):globj(l) {
-#pragma templet  bag_of_tasks.bag_of_tasks
+#pragma templet +bag_of_tasks.bag_of_tasks
         
-#pragma templet /bag_of_tasks.bag_of_tasks
+#pragma templet -bag_of_tasks.bag_of_tasks
         init(); 
 	}
     void resize(unsigned size){
         update(_resize, [&](std::ostream&out) {
-#pragma templet  bag_of_tasks.resize.save
+#pragma templet +bag_of_tasks.resize.save
             
-#pragma templet /bag_of_tasks.resize.save
+#pragma templet -bag_of_tasks.resize.save
 		},
 		[this](std::istream&in, std::ostream&) {
-#pragma templet  bag_of_tasks.resize.update
+#pragma templet +bag_of_tasks.resize.update
             
-#pragma templet /bag_of_tasks.resize.update
+#pragma templet -bag_of_tasks.resize.update
 		});   
     }
     void add(unsigned id,int n){
         update(_add, [&](std::ostream&out) {
-#pragma templet  bag_of_tasks.add.save
+#pragma templet +bag_of_tasks.add.save
             
-#pragma templet /bag_of_tasks.add.save
+#pragma templet -bag_of_tasks.add.save
 		},
 		[this](std::istream&in, std::ostream&) {
-#pragma templet  bag_of_tasks.add.update
+#pragma templet +bag_of_tasks.add.update
             
-#pragma templet /bag_of_tasks.add.update
+#pragma templet -bag_of_tasks.add.update
 		});     
     }
     void put(unsigned id,int nxn){
         update(_put, [&](std::ostream&out) {
-#pragma templet  bag_of_tasks.put.save
+#pragma templet +bag_of_tasks.put.save
             
-#pragma templet /bag_of_tasks.put.save
+#pragma templet -bag_of_tasks.put.save
 		},
 		[this](std::istream&in, std::ostream&) {
-#pragma templet  bag_of_tasks.put.update
+#pragma templet +bag_of_tasks.put.update
             
-#pragma templet /bag_of_tasks.put.update
+#pragma templet -bag_of_tasks.put.update
 		});  
     }
 private:
@@ -73,15 +73,15 @@ private:
         add(0,0);
         put(0,0);
 	}
-#pragma templet  bag_of_tasks
+#pragma templet +bag_of_tasks.class
             
-#pragma templet /bag_of_tasks
+#pragma templet -bag_of_tasks.class
 };
 
-#pragma templet  footer
+#pragma templet +footer
 int main()
 {
     meta_bag_of_tasks meta;
     meta.generate(__FILE__);
 }
-#pragma templet /footer
+#pragma templet -footer
--- lib/todo/meta2.cpp
+++ lib/todo/meta2.cpp
@@ -1,1 +1,109 @@
+#pragma templet +header
+
 #include "meta.hpp"
+#include "acta.hpp"
+
+class meta_bag_of_tasks: public templet::meta::acta {
+public:
+    meta_bag_of_tasks(){
+        name("bag_of_tasks");
+
+        def("master")
+            .in("message", "in");
+
+        def("worker")
+            .start()
+            .out("message", "out");
+    }
+};
+#pragma templet -header
+
+class bag_of_tasks : public templet::acta {
+public:
+    bag_of_tasks(templet::wal&l):templet::acta(l){
+#pragma templet +bag_of_tasks.bag_of_tasks
+        
+#pragma templet -bag_of_tasks.bag_of_tasks
+    }
+public:
+    class message:public templet::acta::message{
+#pragma templet +message
+
+#pragma templet -message
+    };
+    
+    class master:public templet::acta::actor{
+    public:
+        master():templet::acta::actor(){
+#pragma templet +master.master
+            
+#pragma templet -master.master            
+        }
+        master(templet::acta&acta):templet::acta::actor(acta){master();}
+        void init(templet::acta&acta){
+            templet::acta::actor::init(acta);
+#pragma templet +master.init
+            
+#pragma templet -master.init
+        }
+        void in(message&m){ m.srv(this,[this,&m](){on_in(m);}); }
+    private:
+        void on_in(message&m){
+            p_in = &m;
+#pragma templet +master.on_in
+            
+#pragma templet -master.on_in
+        }
+        message* p_in;
+#pragma templet +master.class
+            
+#pragma templet -master.class
+    };
+    
+    class worker:public templet::acta::actor{
+    public:
+        worker():templet::acta::actor(),out(){
+#pragma templet +worker.worker
+            
+#pragma templet -worker.worker
+        }
+        worker(templet::acta&acta):templet::acta::actor(acta,true),out(){worker();}
+        void init(templet::acta&acta){
+            templet::acta::actor::init(acta,true);
+            out.init(this,[this](){on_out(out);});
+#pragma templet +worker.init
+
+#pragma templet -worker.init
+        }
+    private:
+        void on_out(message&m){
+#pragma templet +worker.on_out
+
+#pragma templet -worker.on_out
+        }
+        void on_start()override{
+#pragma templet +worker.on_start
+
+#pragma templet -worker.on_start   
+        }
+        message out;
+#pragma templet +worker.class
+
+#pragma templet -worker.class
+    };
+private:
+    void on_run()override{
+#pragma templet +bag_of_tasks.on_run
+        
+#pragma templet -bag_of_tasks.on_run     
+    }
+#pragma templet +bag_of_tasks.class
+        
+#pragma templet -bag_of_tasks.class     
+};
+
+int main()
+{  
+    meta_bag_of_tasks meta;
+    meta.generate(__FILE__);
+}
